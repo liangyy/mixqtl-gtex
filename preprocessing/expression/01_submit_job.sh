fe70792c6a5e5952f10d342d7b369569a365c03e
Yanyu Liang <yanyul@uchicago.edu>
2020-07-05 15:46:14 -0500

    some minor changes

--- preprocessing/expression/01_submit_job.sh
+++ preprocessing/expression/01_submit_job.sh
@@ -1,8 +1,9 @@
 mkdir -p logs
 
-for row in `cat tissue_metainfo.csv`
+for ii in `seq 1 49`
 do
-  IFS=', ' read -a array <<< "'$row'"
+  row=`cat tissue_metainfo.csv | head -n $ii | tail -n 1`
+  IFS=',' read -a array <<< "$row"
   tissue="${array[0]}"
   tissuestr="${array[1]}"
   echo qsub -N expr-$tissue -v TISSUE=$tissue,TISSUESTR="'$tissuestr'" run.qsub
