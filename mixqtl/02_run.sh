a7b5a14fd0ab1164eb3c63756e821a3ecaaa8f60
Yanyu Liang <yanyul@uchicago.edu>
2020-07-20 10:02:07 -0500

    added more new

--- mixqtl/02_run.sh
+++ mixqtl/02_run.sh
@@ -1,6 +1,21 @@
+#PBS -S /bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l mem=16gb
+#PBS -e logs/run_enrich_${tissue}.err
+#PBS -o logs/run_enrich_${tissue}.out
+
+
+source ~/.bash_profile
+source ~/.bashrc
+
 # ARGS1: tissue name
-tissue=$1
-# conda activate mixqtl
+if [[ ! -z $1 ]]
+then
+  tissue=$1
+fi
+conda activate mixqtl
+
+cd $PBS_O_WORKDIR
 
 function_annotation='/gpfs/data/im-lab/nas40t2/rbonazzola/GTEx/v8/annotations/WGS_Feature_overlap_collapsed.txt.gz'
 mixqtl='/scratch/t.cri.yliang/mixQTL-GTExV8/mixqtl/{tissue}/mixqtl.{tissue}_GTEx_eGene.cis_qtl_pairs.mixQTL.chr{chr_num}.parquet'
@@ -14,17 +29,19 @@ nefine_cs='/scratch/t.cri.yliang/mixQTL-GTExV8/mixfine/{tissue}/mixfine.{tissue}
 cache_dir='/scratch/t.cri.yliang/mixQTL-GTExV8/cache/'
 
 mkdir -p $cache_dir
-
-python functional_enrichment.py \
-  --tissue $tissue \
-  --functional_annotation $function_annotation \
-  --mixqtl $mixqtl \
-  --top_qtl $top_qtl \
-  --strong_gene $strong_gene \
-  --mixfine_pip $mixfine_pip \
-  --nefine_pip $nefine_pip \
-  --mixfine_cs $mixfine_cs \
-  --nefine_cs $nefine_cs \
-  --cache_dir $cache_dir \
-  --output enrichment_$tissue.csv
+if [[ ! -f enrichment/enrichment_$tissue.csv ]]
+then
+  python functional_enrichment.py \
+    --tissue $tissue \
+    --functional_annotation $function_annotation \
+    --mixqtl $mixqtl \
+    --top_qtl $top_qtl \
+    --strong_gene $strong_gene \
+    --mixfine_pip $mixfine_pip \
+    --nefine_pip $nefine_pip \
+    --mixfine_cs $mixfine_cs \
+    --nefine_cs $nefine_cs \
+    --cache_dir $cache_dir \
+    --output enrichment/enrichment_$tissue.csv
+fi
 
