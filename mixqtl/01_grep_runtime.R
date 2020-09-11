258078445aaba5df279ca8d5726f4759fa81e49e
Yanyu Liang <yanyul@uchicago.edu>
2020-09-11 11:23:12 -0500

    added walltime

--- mixqtl/01_grep_runtime.R
+++ mixqtl/01_grep_runtime.R
@@ -17,6 +17,18 @@ grep_runtime = function(tissue) {
   do.call(rbind, df)
 }
 
+grep_walltime = function(tissue) {
+  if(!file.exists(paste0("logs/run_", tissue, ".out"))) {
+    return(NULL)
+  }
+  cmd = paste0("cat logs/run_", tissue, ".out | tail -n 8|head -n 1|awk -F',' '{print $3}' | sed 's#walltime=##g' ")
+  # print(cmd)
+  tmp = read.table(pipe(cmd), header = FALSE, sep = ':')
+  colnames(tmp) = c('hour', 'min', 'sec')
+  tmp$tissue = tissue
+  tmp
+}
+
 df_tissue = read.csv('../preprocessing/expression/tissue_metainfo.csv', header = FALSE)
 df = list()
 for(tissue in df_tissue$V1) {
@@ -25,4 +37,12 @@ for(tissue in df_tissue$V1) {
 df = do.call(rbind, df)
 
 write.table(df, 'mixqtl_runtime.tsv', col = T, row = F, quo = F, sep = '\t')
+
+df = list()
+for(tissue in df_tissue$V1) {
+  df[[length(df) + 1]] = grep_walltime(tissue)
+}
+df = do.call(rbind, df)
+
+write.table(df, 'mixqtl_walltime.tsv', col = T, row = F, quo = F, sep = '\t')
 
