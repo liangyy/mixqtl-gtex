9861dd985594fa5ded767b63f524dcfb179bbf9a
Yanyu Liang <yanyul@uchicago.edu>
2020-09-14 23:08:21 -0500

    added zenodo update

---
+++ mixqtl/03_upload_to_zenodo.sh
@@ -0,0 +1,18 @@
+# submit the list of jobs to upload mixQTL sum stats to zenodo
+
+mkdir zenodo_meta
+
+for tissue in `cat ../preprocessing/expression/tissue_metainfo.csv | cut -f 1 -d,`
+do 
+  cat zenodo_meta_pattern.yaml | sed "s#TISSUE-PLACEHOLDER#$tissue#g" > zenodo_meta/$tissue.yaml
+done
+
+# split tissues into 9 tissues per batch
+cat ../preprocessing/expression/tissue_metainfo.csv | cut -f 1 -d, > zenodo_meta/tissues.txt
+split tissues.txt -l 9 -d zenodo_meta/tissues.txt zenodo_meta/batch
+
+for i in `ls zenodo_meta/batch*`
+do
+  mynum=`echo $i | sed 's#zenodo_meta/batch##g'`
+  qsub -v BATCH=$mynum -N zenodo_$mynum run_upload.qsub
+done
